7f03ad40b26d222df5b70220088b8eea794c29ee
aminashi <amimisia@gmail.com>
2022-04-06 00:29:59 +0100

    Added console project with task 4, part 2 (part 1 is with five zeroes instead of six)

---
+++ task4/task4/main.swift
@@ -0,0 +1,23 @@
+import CryptoKit
+import Foundation
+
+var secret = "iwrupvqb"
+
+//If your secret key is abcdef, the answer is 609043, because the MD5 hash of abcdef609043 starts with five zeroes (000001dbbfa...), and it is the lowest such number to do so.
+
+var found = false
+var number = -1
+var string: String = ""
+var hashmd5: Insecure.MD5.Digest
+var hashString: String
+
+while (!found)
+{
+    (number = number + 1)
+    (string = "\(secret)\(number)")
+    (hashmd5 = Insecure.MD5.hash(data: Data(string.utf8)))
+    (hashString = hashmd5.map { String(format: "%02hhx", $0)}.joined())
+    (found = hashString.starts(with: "000000"))
+}
+
+print("the number is: \(number)")
