e6c189e1e8fcb3e339e8d2eec9c65e92335ea37a
aminashi <amimisia@gmail.com>
2022-04-09 19:05:51 +0100

    Added bitwise operations

--- task4/tasks/task7.swift
+++ task4/tasks/task7.swift
@@ -7,13 +7,13 @@ enum Operations: String, CaseIterable {
     case rshift = "RSHIFT"
     case not = "NOT"
     
-    func execute(_ equation: [Int]) -> Int {
+    func execute(_ equation: [UInt16]) -> UInt16 {
         switch self {
-        case .not: return equation[0] * 2
-        case .and: return equation.reduce(0, {$0+$1})
-        case .or: return equation.reduce(0, {$0-$1})
-        case .lshift: return equation.reduce(0, {$0/$1})
-        case .rshift: return equation.reduce(0, {$0&$1})
+        case .not: return ~equation[0]
+        case .and: return equation[0]&equation[1]
+        case .or: return equation[0]|equation[1]
+        case .lshift: return equation[0]<<equation[1]
+        case .rshift: return equation[0]>>equation[1]
         }
     }
     
@@ -37,7 +37,7 @@ func task7() {
     NOT y -> i
     """.components(separatedBy: "\n")
     
-    var elements: [String : Int] = [:]
+    var elements: [String : UInt16] = [:]
     
     for line in circuit {
         let data = line.components(separatedBy: " -> ")
@@ -49,13 +49,15 @@ func task7() {
             if (equation.contains(operation.separator)) {
                 var equationString = equation.components(separatedBy: operation.separator)
                 currentOperator = operation
-                var equationList: [Int] = []
+                var equationList: [UInt16] = []
                 if let val1 = elements[equationString[0]] {
-                    equationList.append(Int(val1))
+                    equationList.append(UInt16(val1))
                 }
                 
                 if let val2 = elements[equationString[1]] {
-                    equationList.append(Int(val2))
+                    equationList.append(UInt16(val2))
+                } else {
+                    equationList.append(UInt16(equationString[1])!)
                 }
                 
                 elements[newElement] = operation.execute(equationList)
@@ -64,7 +66,7 @@ func task7() {
         }
         
         if currentOperator == nil {
-            elements[newElement] = Int(equation)!
+            elements[newElement] = UInt16(equation)!
         }
     }
     print(elements)
