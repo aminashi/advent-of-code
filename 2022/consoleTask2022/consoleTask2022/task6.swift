4ab9b36973dfa9c93c07f6c6f84f87d86c094fa4
aminashi <sufka@protonmail.ch>
2022-12-29 21:02:56 +0000

    Added task6 file with input data

---
+++ 2022/consoleTask2022/consoleTask2022/task6.swift
@@ -0,0 +1,13 @@
+import Foundation
+
+func task6() {
+    
+    let input = """
+    mjqjpqmgbljsphdztnvjfqwrcgsmlb
+    bvwbjplbgvbhsrlpgdmjqwftvncz
+    nppdvjthqldpwncqszvftbrmjlhg
+    nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg
+    zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw
+    """.components(separatedBy: "\n")
+
+}
