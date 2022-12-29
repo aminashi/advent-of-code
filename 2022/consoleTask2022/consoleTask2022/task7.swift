8c348f4b951b5adc4b303017ee6ede10abbf2a94
aminashi <sufka@protonmail.ch>
2022-12-29 23:36:19 +0000

    Added first part of task 7 - processing commands

--- 2022/consoleTask2022/consoleTask2022/task7.swift
+++ 2022/consoleTask2022/consoleTask2022/task7.swift
@@ -1,6 +1,63 @@
 import Foundation
 
+enum Command {
+    case cd(name: String)
+    case ls
+    case back
+    
+    init?(line: String) {
+        if line == "$ ls" {
+            self = Command.ls
+        } else if line == "$ cd .." {
+            self = Command.back
+        } else {
+            let match = line.firstMatch(of: /\$ cd (.+)/)
+            self = Command.cd(name: String(match!.output.1))
+        }
+    }
+    
+    func rawValue() -> String {
+        switch self {
+        case .cd(let name): return "$ cd \(name)"
+        case .ls: return "$ ls"
+        case .back: return "$ cd .."
+        }
+    }
+}
+
+protocol Element {
+    var name: String { get }
+    var size: Int { get }
+}
+
+class Directory: Element {
+    var name: String
+    var elements: [Element]
+    var size: Int
+    var parent: Directory?
+    
+    init(name: String, parent: Directory? = nil) {
+        self.name = name
+        self.elements = []
+        self.size = 0
+        self.parent = parent
+    }
+}
+
+class File: Element {
+    var name: String
+    var size: Int
+    
+    init(name: String, size: Int) {
+        self.name = name
+        self.size = size
+    }
+}
+
 func task7() {
+    
+    var structure: Directory? = nil
+    var pointing: Directory? = nil
     
     let input = """
     $ cd /
@@ -26,5 +83,26 @@ func task7() {
     8033020 d.log
     5626152 d.ext
     7214296 k
-    """
+    """.components(separatedBy: "\n")
+    
+    for line in input {
+        if line.first == "$" {
+            let command = Command(line: line)
+            if case let .cd(name) = command {
+                if structure != nil {
+                    pointing = pointing?.elements.first(where: {$0.name == name}) as? Directory
+                } else {
+                    structure = Directory(name: name)
+                    pointing = structure
+                }
+            }
+            if case .ls = command {
+                continue
+            }
+            if case .back = command {
+                pointing = pointing?.parent
+            }
+        } else {
+    }
+}
 }
