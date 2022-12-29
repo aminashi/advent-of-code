75ec8ebadee188a088954c0dd83cda9607fb5162
aminashi <sufka@protonmail.ch>
2022-12-29 21:03:06 +0000

    Added task 6 part 1 solution

--- 2022/consoleTask2022/consoleTask2022/task6.swift
+++ 2022/consoleTask2022/consoleTask2022/task6.swift
@@ -10,4 +10,18 @@ func task6() {
     zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw
     """.components(separatedBy: "\n")
 
+    let windowSize = 4
+    
+    for line in input {
+        var loopCount = 0
+        var uniqueLetters: Set = Set<Character>()
+        while uniqueLetters.count != 4 {
+            let startIndex = line.index(line.startIndex, offsetBy: loopCount)
+            let endIndex = line.index(line.startIndex, offsetBy: windowSize + loopCount - 1)
+            var window = String(line[startIndex...endIndex])
+            uniqueLetters = Set(window)
+            loopCount += 1
+        }
+        print(loopCount+3)
+    }
 }
