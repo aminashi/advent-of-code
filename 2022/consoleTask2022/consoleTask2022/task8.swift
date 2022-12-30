86225268a9786594fde6146efc12d803f0df2275
aminashi <sufka@protonmail.ch>
2022-12-30 14:17:03 +0000

    First iteration (not quite right as it's not checking ALL trees in a line, only neighbours)

--- 2022/consoleTask2022/consoleTask2022/task8.swift
+++ 2022/consoleTask2022/consoleTask2022/task8.swift
@@ -2,13 +2,45 @@ import Foundation
 
 func task8() {
     
-    let input = """
+    let grid: [[Int]] = """
     30373
     25512
     65332
     33549
     35390
-    """.components(separatedBy: "\n").map{ Array($0) }
+    """.components(separatedBy: "\n").map{ line in
+        line.compactMap({ Int(String($0)) })
+    }
     
+    var visibleTrees = 4*(grid.count-1)
     
+    for (rowIndex, row) in grid.enumerated() {
+        if rowIndex == 0 || rowIndex == grid.count - 1 {
+            continue
+        }
+        
+        for (columnIndex, tree) in row.enumerated() {
+            if columnIndex == 0 || columnIndex == row.count - 1 {
+                continue
+            }
+            
+                visibleTrees += 1
+                print("\(rowIndex), \(columnIndex), \(tree), \(row[columnIndex - 1] < tree) \(row[columnIndex + 1] < tree) \(grid[rowIndex-1][columnIndex] < tree) \(grid[rowIndex+1][columnIndex] < tree)")
+            }
+            
+        }
+    }
+    
+    print(visibleTrees)
+}
+
+    }
+    
+    func column() -> Int {
+        switch self {
+        case .top: return -1
+        case .bottom: return 1
+        default: return 0
+        }
+    }
 }
