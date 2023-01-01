9dda38bd83e4bd37f9894cff9e7ac0404dedb573
aminashi <sufka@protonmail.ch>
2023-01-01 16:50:09 +0000

    Added switch to the main advent function

--- 2022/consoleTask2022/consoleTask2022/advent.swift
+++ 2022/consoleTask2022/consoleTask2022/advent.swift
@@ -22,5 +22,15 @@ public protocol AdventProtocol {
 
 public class Advent: AdventProtocol {
     public func task(number: Number, part: Part) {
+        switch number {
+            case .one: return task1(part: part)
+        case .two: return task2(part: part)
+        case .three: return task3(part: part)
+        case .four: return task4(part: part)
+        case .five: return task5(part: part)
+        case .six: return task6(part: part)
+        case .seven: return task7(part: part)
+        case .eight: return task8(part: part)
+        }
     }
 }
