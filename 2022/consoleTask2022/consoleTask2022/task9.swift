67f2fe1ae646ce2ddb4e0fc497699ab31a097980
aminashi <sufka@protonmail.ch>
2023-01-27 14:52:47 +0000

    Added a solution for task 9 part 1

--- 2022/consoleTask2022/consoleTask2022/task9.swift
+++ 2022/consoleTask2022/consoleTask2022/task9.swift
@@ -3,10 +3,22 @@ import Foundation
 extension Advent {
     func task9(part: Part) {
         let input = Number.nine.input.components(separatedBy: "\n")
+        
+        var headPosition: Point = Point(x: 0, y: 0)
+        var tailPosition: Point = Point(x: 0, y: 0)
+        var tailPath: [Point: Bool] = [Point(x: 0, y: 0) : true]
+        
         for line in input {
             let instruction = line.components(separatedBy: " ")
-            if let direction = Instruction(rawValue: instruction[0]) {
-                
+            if let direction = Instruction(rawValue: instruction[0]), let steps = Int(instruction[1]) {
+                for _ in 0...steps-1 {
+                    var newHeadPosition = headPosition.move(to: direction)
+                    if (!tailPosition.isInRange(of: newHeadPosition)) {
+                        tailPosition = headPosition
+                        tailPath[tailPosition] = true
+                    }
+                    headPosition = newHeadPosition
+                }
             }
         }
     }
@@ -17,4 +29,25 @@ enum Instruction: String {
     case L
     case U
     case D
+}
+
+struct Point: Hashable {
+    var x: Int
+    var y: Int
+    
+    func move(to direction: Instruction) -> Point {
+        switch direction {
+        case .D: return Point(x: self.x - 1, y: self.y)
+        case .U: return Point(x: self.x + 1, y: self.y)
+        case .L: return Point(x: self.x, y: self.y - 1)
+        case .R: return Point(x: self.x, y: self.y + 1)
+        }
+    }
+    
+    func isInRange(of point: Point) -> Bool {
+        if (point.x - self.x)*(point.x - self.x) > 1 || (point.y - self.y)*(point.y - self.y) > 1 {
+            return false
+        }
+        return true
+    }
 }
